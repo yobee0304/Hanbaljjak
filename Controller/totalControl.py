04c116f5a63b4de3ebab3bd3fc0d03863d47d90b
gmAnn00 <40567670+gmAnn00@users.noreply.github.com>
2020-05-08 06:47:29 +0900

    API1, API3, 모델 임시데이터
    API3 할거있음

--- Controller/totalControl.py
+++ Controller/totalControl.py
@@ -1,4 +1,33 @@
+from models import Phoneme, Result, Record, Sentence
+from database import db_session
 
 # API3
+"""
+사용자의 전체적인 결과값들을 클라이언트에 전송한다. 
+결과값은 가장 많이 틀린 음소(umb 포함?) 최대 3개, 
+그에 맞는 추천 문장 각각 1개, 
+최근에 연습한 문장의 일치율 최대 5개이다.
+"""
 def totalControl():
     return 'total success'
+	
+def totalResult():
+    most_lst = []
+	recommend_lst = []
+	recent_lst = []
+
+	# 가장 많이 틀린 음소(umb 포함?) 최대 3개
+	for most in db_session.query(Record).order_by(Record.count.desc()).limit(3):
+		#most_lst.append([most.recordId, most.recordData, most.type, most.count])
+		most_lst.append(most.recordData)
+
+	# 많이 틀린 음소 3개의 추천문장 1개씩
+	# TODO
+
+	#최근에 연습한 문장의 일치율 최대 5개
+	for practice in db_session.query(Result).order_by(Result.date).limit(5):
+		recent_lst.append(practice.score)
+
+
+	return most_lst, recommend_lst, recent_lst
+	
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, VARCHAR, CHAR, Float, DateTime, ForeignKey
-from database import Base
+from database import Base, db_session
 
 import datetime
 
@@ -8,6 +8,15 @@ class Sentence(Base):
     sentenceId = Column(Integer, primary_key=True)
     sentenceData = Column(VARCHAR(50))
     standard = Column(VARCHAR(50))
+	
+	def __init__(self, stid, stdata, standard):
+		self.sentenceId = stid
+		self.sentenceData = stdata
+		self.standard = standard
+
+    def __repr__(self):
+        return "<Sentence('%d', '%s', '%s')>" % (self.sentenceId, self.sentenceData, self.standard)
+
 
 class Phoneme(Base):
     __tablename__ = 'phoneme'
@@ -15,6 +24,17 @@ class Phoneme(Base):
     sentenceId = Column(ForeignKey('sentence.sentenceId'))
     phonemeData = Column(CHAR(10))
     type = Column(CHAR(10))
+	
+    def __init__(self, phid, stid, phdata, type):
+        self.phonemeId=phid
+        self.sentenceId = stid
+        self.phonemeData = phdata
+        self.type = type
+
+    def __repr__(self):
+        return "<Phoneme('%d', '%d', '%s', '%s')>" \
+               % (self.phonemeId, self.sentenceId, self.phonemeId, self.type)
+
 
 class Result(Base):
     __tablename__ = 'result'
@@ -23,6 +43,18 @@ class Result(Base):
     resultData = Column(VARCHAR(50))
     score = Column(Float)
     date = Column(DateTime, default=datetime.datetime.utcnow())
+	
+    def __init__(self, rsid, stid, rsdata, score):
+        self.resultId = rsid
+        self.sentenceId = stid
+        self.resultData = rsdata
+        self.score = score
+        self.date = datetime.datetime.utcnow()
+
+    def __repr__(self):
+        return "<Result('%d', '%d', '%s', '%f')>" \
+               % (self.resultId, self.sentenceId, self.resultId, self.score)
+
 
 class Record(Base):
     __tablename__ = 'record'
@@ -30,3 +62,51 @@ class Record(Base):
     recordData = Column(CHAR(10))
     type = Column(CHAR(10))
     count = Column(Integer, default=0)
+	
+    def __init__(self, rcid, rcdata, type, count):
+        self.recordId = rcid
+        self.recordData = rcdata
+        self.type = type
+        self.count = count
+
+    def __repr__(self):
+        return "<Record('%d', '%s', '%s', '%d')>" \
+               % (self.recordId, self.recordData, self.type, self.count)
+			   
+			   
+# 임시 데이터
+"""
+s1 = Sentence(1, "날씨가 맑다", "날씨가 막따")
+db_session.add(s1)
+s2 = Sentence(2, "공권력", "공꿘녁")
+db_session.add(s2)
+db_session.commit()
+
+p1 = Phoneme(11, 1, 'ㄱ', 'b')
+db_session.add(p1)
+p2 = Phoneme(12, 1, 'ㄸ', 'u')
+db_session.add(p2)
+p3 = Phoneme(13, 2, 'ㄲ', 'u')
+db_session.add(p3)
+p4 = Phoneme(14, 2, 'ㄴ', 'u')
+db_session.add(p4)
+db_session.commit()
+
+rs1 = Result(21, 1, "날씨가 말따", 86)
+db_session.add(rs1)
+rs2 = Result(22, 2, "공궐력", 60)
+db_session.add(rs2)
+db_session.commit()
+
+rc1 = Record(31, 'ㄱ', 'b', '1')
+db_session.add(rc1)
+rc2 = Record(32, 'ㄲ', 'u', '1')
+db_session.add(rc2)
+rc3 = Record(33, 'ㄴ', 'b', '1')
+db_session.add(rc3)
+rc4 = Record(34, 'ㄴ', 'u', '1')
+db_session.add(rc4)
+rc5 = Record(35, 'ㄱ', 'u', '2')
+db_session.add(rc5)
+db_session.commit()
+"""
