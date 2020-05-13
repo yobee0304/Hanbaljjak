732e40df2e9d1c44549c2fc6f2bfcfa6190b0d32
gmAnn00 <40567670+gmAnn00@users.noreply.github.com>
2020-05-14 01:14:50 +0900

    API 4 update

--- Controller/customerSentenceControl.py
+++ Controller/customerSentenceControl.py
@@ -3,4 +3,13 @@ from models import Phoneme, Result, Record, Sentence
 from database import db_session
 
 def customerSentenceControl():
-    return "customerSentenceControl success"
+    cust_sentence_lst = []
+    cust_sentence_dict = {"sentenceId": 0, "sentenceData": "", "standard": ""}
+
+    for sen in db_session.query(Sentence).order_by(Sentence.sentenceId).filter(Sentence.check == True):
+        cust_sentence_dict["sentenceId"] = sen.sentenceId
+        cust_sentence_dict["sentenceData"] = sen.sentenceData
+        cust_sentence_dict["standard"] = sen.standard
+        cust_sentence_lst.append(cust_sentence_dict.copy())
+
+    return json.dumps(cust_sentence_lst, ensure_ascii=False)
