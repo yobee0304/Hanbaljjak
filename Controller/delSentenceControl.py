4800ec7b92613eba2174504bd0200aa63192df15
gmAnn00 <40567670+gmAnn00@users.noreply.github.com>
2020-05-17 18:51:23 +0900

    API 6 수정

--- Controller/delSentenceControl.py
+++ Controller/delSentenceControl.py
@@ -1,15 +1,17 @@
-from models import Phoneme, Result, Sentence
+from models import Word, Result, Sentence
 from database import db_session
 from flask import request
 
-
+# API 6
+# <연습 문장 삭제>
+# senteceId로 sentece, result, word 테이블 데이터 삭제
 def delSentenceControl():
     if (request.method == 'POST'):
 
         sentenceId = request.form['sentenceId']
 
-        # Phoneme table 에서 해당 sentenceId row 삭제
-        db_session.query(Phoneme).filter(Phoneme.sentenceId == sentenceId). \
+        # Word table 에서 해당 sentenceId row 삭제
+        db_session.query(Word).filter(Word.sentenceId == sentenceId).\
             delete(synchronize_session=False)
         # Result table 에서 해당 sentenceId row 삭제
         db_session.query(Result).filter(Result.sentenceId == sentenceId). \
