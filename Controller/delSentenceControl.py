a8675ffd50e4854addb4533898c1659ef9280634
gmAnn00 <40567670+gmAnn00@users.noreply.github.com>
2020-05-23 00:27:28 +0900

    API 6, API 9 수정
    삭제할 id list로

--- Controller/delSentenceControl.py
+++ Controller/delSentenceControl.py
@@ -2,23 +2,25 @@ from models import Word, Result, Sentence
 from database import db_session
 from flask import request
 
+
 # API 6
 # <연습 문장 삭제>
-# senteceId로 sentece, result, word 테이블 데이터 삭제
+# sentence_id로 sentence, result, word 테이블 데이터 삭제
 def delSentenceControl():
     if (request.method == 'POST'):
+        sentence_id_lst = request.form.getlist('sentenceId')
 
-        sentenceId = request.form['sentenceId']
+        for sentence_id in sentence_id_lst:
+            # Word table 에서 해당 sentenceId row 삭제
+            db_session.query(Word).filter(Word.sentenceId == sentence_id).\
+                delete(synchronize_session=False)
+            # Result table 에서 해당 sentenceId row 삭제
+            db_session.query(Result).filter(Result.sentenceId == sentence_id). \
+                delete(synchronize_session=False)
+            # Sentence table 에서 해당 sentenceId row 삭제
+            db_session.query(Sentence).filter(Sentence.sentenceId == sentence_id). \
+                delete(synchronize_session=False)
 
-        # Word table 에서 해당 sentenceId row 삭제
-        db_session.query(Word).filter(Word.sentenceId == sentenceId).\
-            delete(synchronize_session=False)
-        # Result table 에서 해당 sentenceId row 삭제
-        db_session.query(Result).filter(Result.sentenceId == sentenceId). \
-            delete(synchronize_session=False)
-        # Sentence table 에서 해당 sentenceId row 삭제
-        db_session.query(Sentence).filter(Sentence.sentenceId == sentenceId). \
-            delete(synchronize_session=False)
-        db_session.commit()
+            db_session.commit()
 
     return "delSentenceControl success"
