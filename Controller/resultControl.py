c5280ab447130f18ff46a2eee482f88383857c79
gmAnn00 <40567670+gmAnn00@users.noreply.github.com>
2020-05-29 16:00:15 +0900

    API2 수정
    Word.type->Word.wordType

--- Controller/resultControl.py
+++ Controller/resultControl.py
@@ -26,17 +26,17 @@ def phonemeConvert(word):
 ###############################################################
 
 ################# Googel Speech-to-Text API ###################
-
+"""
 from google.cloud import speech_v1
 from google.cloud.speech_v1 import enums
 import io
 
 def sample_recognize(local_file_path):
-    """
-    Transcribe a short audio file using synchronous speech recognition
-    Args:
-      local_file_path Path to local audio file, e.g. /path/audio.wav
-    """
+    
+    #Transcribe a short audio file using synchronous speech recognition
+    #Args:
+    #  local_file_path Path to local audio file, e.g. /path/audio.wav
+    
 
     client = speech_v1.SpeechClient()
 
@@ -78,7 +78,7 @@ def sample_recognize(local_file_path):
         for i in range(0, maxalt):
             alternative = result.alternatives[i]
             print(u"Transcript: {}".format(alternative.transcript))
-
+"""
 ###############################################################
 
 ########################## API2 ###############################
@@ -218,7 +218,7 @@ def resultControl():
         for type in Wrong_pho_dict:
             for pho in Wrong_pho_dict[type]:
                 # print(pho)
-                updateData = db_session.query(Record).filter(Record.type == type)\
+                updateData = db_session.query(Record).filter(Record.recordType == type)\
                                                     .filter(Record.recordData == pho).first()
                 # print(updateData.type, updateData.recordData)
                 updateData.count += Wrong_pho_dict[type][pho]
@@ -319,7 +319,7 @@ def resultControl():
 
             for random_select_word in Wrong_word_list:
                 Word_query = db_session.query(Word).filter(Word.wordData == random_select_word[0])\
-                    .filter(Word.type == random_select_word[1]).filter(Word.sentenceId != sentenceId)
+                    .filter(Word.wordType == random_select_word[1]).filter(Word.sentenceId != sentenceId)
                 Word_entry = [pq.sentenceId for pq in Word_query]
 
                 if Word_entry:
@@ -344,5 +344,3 @@ def resultControl():
     )
 
 ###############################################################
-
-
