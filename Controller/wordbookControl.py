7b8ca8a8017c50cf2a8393b4f1b2b2cedd52e8f7
gmAnn00 <40567670+gmAnn00@users.noreply.github.com>
2020-06-09 17:17:18 +0900

    API 7 모든 추천 문장 반환

--- Controller/wordbookControl.py
+++ Controller/wordbookControl.py
@@ -8,14 +8,14 @@ import random
 # wordbook 테이블에 있는 모든 wordbookId와 wordbookData, 추천 문장 object 1개 반환
 def wordbookControl():
     wordbook_lst = []    # wordbookId, wordbookData, 추천 문장 object 저장
+    sentence_dict_lst = []
     recommend_sen_id_lst = []    # 추천 문장 후보 ID 저장
     sentence_dict = {"sentenceId" : 0, "sentenceData" : "", "standard" : ""}
-    wordbook_dict = {"wordbookId" : 0, "wordData" : "", "recommend" : sentence_dict}
+    wordbook_dict = {"wordbookId" : 0, "wordData" : "", "recommend" : sentence_dict_lst}
 
     # wordbook 테이블에서 wordbookId와 wordData 가져오기
     for wordbook_entry in db_session.query(WordBook).order_by(WordBook.wordbookId):
         wordbook_dict["wordbookId"] = wordbook_entry.wordbookId
-        #wordbook_dict["wordData"] = wordbook_entry.wordData
 
         # wordData가 같은 sentenceId(=추천 문장 후보 ID) 가져오기
         for word_entry in db_session.query(Word).\
@@ -30,6 +30,7 @@ def wordbookControl():
                 wordbook_dict["wordData"] = wordbook_entry.wordData
 
         # 추천문장 후보 ID list(recommend_sen_id_lst)가 empty가 아니면 랜덤으로 하나 선택
+        """
         if recommend_sen_id_lst:
             sentence_dict["sentenceId"] = random.choice(recommend_sen_id_lst)
             # 선택된 sentenceId로 sentenceData, standard 가져오기
@@ -37,6 +38,16 @@ def wordbookControl():
                     filter(Sentence.sentenceId == sentence_dict["sentenceId"]):
                 sentence_dict["sentenceData"] = sen_entry.sentenceData
                 sentence_dict["standard"] = sen_entry.standard
+        """
+        # 단어장의 단어가 포함된 모든 추천 문장 반환
+        if recommend_sen_id_lst:
+            for sen_id_entry in recommend_sen_id_lst:
+                for sen_entry in db_session.query(Sentence).\
+                        filter(Sentence.sentenceId == sen_id_entry):
+                    sentence_dict["sentenceId"] = sen_id_entry
+                    sentence_dict["sentenceData"] = sen_entry.sentenceData
+                    sentence_dict["standard"] = sen_entry.standard
+                sentence_dict_lst.append(sentence_dict.copy())
 
         # wordbook 테이블에 있는 단어가 word, sentence 테이블에는 없는 경우
         # wordbook 테이블에 있는 그대로 반환하고
@@ -46,9 +57,11 @@ def wordbookControl():
             sentence_dict["sentenceData"] = ""
             sentence_dict["standard"] = ""
             wordbook_dict["wordData"] = wordbook_entry.wordData
+            sentence_dict_lst.append(sentence_dict.copy())
 
-        wordbook_dict["recommend"] = sentence_dict.copy()
+        wordbook_dict["recommend"] = sentence_dict_lst.copy()
         wordbook_lst.append(wordbook_dict.copy())
         recommend_sen_id_lst = []
+        sentence_dict_lst = []
 
     return json.dumps(wordbook_lst, ensure_ascii=False)
