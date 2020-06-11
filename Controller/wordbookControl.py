5d88e7a8372861bb0da830d3dc6c13042c40ceaf
gmAnn00 <40567670+gmAnn00@users.noreply.github.com>
2020-06-12 01:07:35 +0900

    API7 수정
    단어장에 단어 그대로 저장하고 word 테이블에서 검색할 때는 konlpy 로 분석한다

--- Controller/wordbookControl.py
+++ Controller/wordbookControl.py
@@ -1,18 +1,19 @@
 import json
 from models import WordBook, Word, Sentence
 from database import db_session
-import random
+from konlpy.tag import Hannanum
 
 
 # API 7
 # wordbook 테이블에 있는 모든 wordbookId와 wordbookData, 추천 문장 object 1개 반환
 def wordbookControl():
     wordbook_lst = []    # wordbookId, wordbookData, 추천 문장 object 저장
-    sentence_dict_lst = []
+    sentence_dict_lst = []    # 추천 문장 object 들을 저장
     recommend_sen_id_lst = []    # 추천 문장 후보 ID 저장
     sentence_dict = {"sentenceId" : 0, "sentenceData" : "", "standard" : ""}
     wordbook_dict = {"wordbookId" : 0, "wordData" : "", "recommend" : sentence_dict_lst}
-
+    
+    """
     # wordbook 테이블에서 wordbookId와 wordData 가져오기
     for wordbook_entry in db_session.query(WordBook).order_by(WordBook.wordbookId):
         wordbook_dict["wordbookId"] = wordbook_entry.wordbookId
@@ -30,7 +31,7 @@ def wordbookControl():
                 wordbook_dict["wordData"] = wordbook_entry.wordData
 
         # 추천문장 후보 ID list(recommend_sen_id_lst)가 empty가 아니면 랜덤으로 하나 선택
-        """
+        
         if recommend_sen_id_lst:
             sentence_dict["sentenceId"] = random.choice(recommend_sen_id_lst)
             # 선택된 sentenceId로 sentenceData, standard 가져오기
@@ -39,6 +40,24 @@ def wordbookControl():
                 sentence_dict["sentenceData"] = sen_entry.sentenceData
                 sentence_dict["standard"] = sen_entry.standard
         """
+    # wordbook 테이블에서 wordbookId와 wordData 가져오기
+    for wordbook_entry in db_session.query(WordBook).order_by(WordBook.wordbookId):
+        wordbook_dict["wordbookId"] = wordbook_entry.wordbookId
+        wordbook_dict["wordData"] = wordbook_entry.wordData
+        word_data = ""    # konlpy로 분석한 wordbookData (N(체언) 또는'다'가 빠진 P(용언))
+        han = Hannanum()
+        wordbook_analyze = han.pos(wordbook_entry.wordData)
+        #print(wordbook_analyze)
+
+        # P(용언), N(체언)인 단어를 wordData에 저장
+        for wordbook_analyze_entry in wordbook_analyze:
+            if wordbook_analyze_entry[1] == 'P' or wordbook_analyze_entry[1] == 'N':
+                word_data = wordbook_analyze_entry[0]
+
+        # wordData가 같은 sentenceId(=추천 문장 후보 ID) 가져오기
+        for word_entry in db_session.query(Word).filter(Word.wordData == word_data):
+            recommend_sen_id_lst.append(word_entry.sentenceId)
+
         # 단어장의 단어가 포함된 모든 추천 문장 반환
         if recommend_sen_id_lst:
             for sen_id_entry in recommend_sen_id_lst:
